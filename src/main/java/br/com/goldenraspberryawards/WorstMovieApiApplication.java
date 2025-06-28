a2e2cc933e3a16096c005f1fe5c7d18e15ba82f6
Diego Gomes Borges <diegogb2013@gmail.com>
2025-06-28 10:32:07 -0300

    feat: creating the project to API REST

---
+++ src/main/java/br/com/goldenraspberryawards/WorstMovieApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.goldenraspberryawards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorstMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorstMovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/goldenraspberryawards/WorstMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.goldenraspberryawards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorstMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
